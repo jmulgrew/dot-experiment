83b0f5248de187619f3026011c05adf2c3119b4f
jmulgrew <36007312+jmulgrew@users.noreply.github.com>
2019-04-29 16:57:03 -0400

    Added function explanations

--- stimulus.py
+++ stimulus.py
@@ -7,8 +7,12 @@ class stimulus:
         self.curr_item = 0
         self.paused = False
         self.while_paused = while_paused
-        
+
     def expose(self):
+        '''Reveals the next item in the array (whether image or sound) as long
+        as the experiment is not paused. It then increments the current item
+        to continue going through the array.
+        '''
         item = self.array[self.curr_item]
         if item is not None:
             if not self.paused or self.while_paused:
