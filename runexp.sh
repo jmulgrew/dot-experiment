2f0c8bbd8a14fc43cbad5df89fe1644837a38efb
jmulgrew <36007312+jmulgrew@users.noreply.github.com>
2019-06-06 13:16:51 -0400

    Corrected image folder names

--- runexp.sh
+++ runexp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-chmod +x runexp.sh
-cd Documents/GitHub/dot-experiment
+# echo "To run the script you'll need to give permission in your terminal"
+# chmod +x path-to-script/<script>.sh
+
+cd ~/Users/jerrica/Documents/GitHub/dot-experiment
 python experiment.py
